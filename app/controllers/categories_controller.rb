0e56b1e1e22d1129e1df9be374fa6e326dc8e3ff
arielmestrada <estrada.ariel.m@gmail.com>
2022-10-12 09:22:53 +0800

    CRUD complete for category and service

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,26 @@
 class CategoriesController < ApplicationController
     before_action :authenticate_user!
+    before_action :ensure_frame_response, only: [:new, :edit]
 
     def index
         @categories = Category.all
+    end
+
+    def show        
+        redirect_to categories_path
+    end
+
+    def new
         @category = Category.new
+    end
+
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id]).update(category_params)
+        redirect_to categories_path
     end
 
     def create
@@ -11,9 +28,18 @@ class CategoriesController < ApplicationController
         redirect_to categories_path
     end
 
+    def destroy
+        @category = Category.find(params[:id]).destroy
+        redirect_to categories_path
+    end
+
     private
 
     def category_params
         params.require(:category).permit(:name, :description)
+    end
+
+    def ensure_frame_response
+        redirect_to categories_path unless turbo_frame_request?
     end
 end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,14 +1,36 @@
 class ServicesController < ApplicationController
   before_action :authenticate_user!
   before_action :get_category
+  before_action :get_service, only: [:edit, :update, :destroy]
+  before_action :ensure_frame_response, only: [:new, :edit]
 
   def index
     @services = @category.services
-    @service = Service.new
+  end
+    
+  def show        
+    redirect_to category_services_path(@category)
+  end
+  
+  def new
+    @service = @category.services.new
+  end
+  
+  def edit
   end
 
   def create
     @service = @category.services.create(service_params)
+    redirect_to category_services_path(params[:category_id])
+  end
+
+  def update
+    @service.update(service_params)
+    redirect_to category_services_path(params[:category_id])
+  end
+
+  def destroy
+    @service.destroy
     redirect_to category_services_path(params[:category_id])
   end
 
@@ -20,6 +42,14 @@ class ServicesController < ApplicationController
 
   def get_category
     @category = Category.find(params[:category_id])
+  end
+
+  def get_service
+    @service = @category.services.find(params[:id])
+  end
+
+  def ensure_frame_response
+    redirect_to category_services_path(@category) unless turbo_frame_request?
   end
 
 end
