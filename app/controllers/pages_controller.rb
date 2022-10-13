78b1f8fcc4c04d0dc621ac4f1efabec07f018c42
arielmestrada <estrada.ariel.m@gmail.com>
2022-10-13 19:45:55 +0800

    design WIP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,21 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:overview]
 
   def home
   end
 
   def overview
+    @count = Hash.new
+    Booking.all.each do |booking|
+      if @count.include?(booking.service_name)
+        @count[booking.service_name] += 1
+      else
+        @count[booking.service_name] = 1
+      end
+    end
+    @most_booked_service = @count.max_by { |k, v| v }[0]
+    @users = User.all
+    @bookings = Booking.all
   end
 
 end
